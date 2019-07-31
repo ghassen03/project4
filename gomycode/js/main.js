83474b21aad31d05a4e5fc5b41598e32c83ee2a4
ghassen03 <52958257+ghassen03@users.noreply.github.com>
2019-07-31 16:19:14 +0200

    Add files via upload

---
+++ gomycode/js/main.js
@@ -0,0 +1,26 @@
+ function bolde(){
+     document.getElementById("texte").style.fontWeight="bold";
+ }
+ var getid=document.getElementById("texte");
+ function x () {
+     getid.style.fontStyle="italic";
+ }
+ function d () {
+     getid.style.textDecoration="underlined"
+ }
+ function fontSize() {
+    getid.style.fontSize=document.getElementById("52").value;
+ }
+ function police () {
+     getid.style.fontFamily= document.getElementById("55").value;;
+ }
+ $(".bt").hide();
+ $(".photos").mouseenter(function(){
+     $(this).css("opacity","0.5");
+     $(this).find(".bt").show();
+ });
+ $(".p").mousel$(thiseave(function(){
+    $(this).css("opacity","1");
+    $(this).find(".bt").hide();
+ })
+ 
